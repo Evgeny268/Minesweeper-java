c6df2ea538878c29a6eaf33eaeeecf8f71b60ad0
Evgeny268 <evgepishcin@gmail.com>
2019-11-08 21:35:32 +0300

    implemented class FieldCell

--- src/gameLogic/FieldCell.java
+++ src/gameLogic/FieldCell.java
@@ -1,8 +1,40 @@
 package gameLogic;
 
 public class FieldCell {
-    protected boolean opened;
-    protected boolean mined;
-    protected boolean flagged;
-    protected int minesAround;
+    protected boolean opened = false;
+    protected boolean mined = false;
+    protected boolean flagged = false;
+    protected int minesAround = 0;
+
+    public FieldCell(boolean mined) {
+        this.mined = mined;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+    public void openCell(){
+        opened = true;
+    }
+
+    public boolean isMined() {
+        return mined;
+    }
+
+    public boolean isFlagged() {
+        return flagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        this.flagged = flagged;
+    }
+
+    public int getMinesAround() {
+        return minesAround;
+    }
+
+    public void setMinesAround(int minesAround) {
+        this.minesAround = minesAround;
+    }
 }
